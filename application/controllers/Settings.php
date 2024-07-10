1eb35d54b30914d15fb3b7e49a35ae04e5af8f57
gihantha <gihantha01@gmail.com>
2024-07-10 12:53:41 +0530

    User profile page and controller created

---
+++ application/controllers/Settings.php
@@ -0,0 +1,11 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Settings extends CI_Controller {
+
+	
+    
+	function user_profile(){
+        $this->load->view("user_profile");
+    }
+}
